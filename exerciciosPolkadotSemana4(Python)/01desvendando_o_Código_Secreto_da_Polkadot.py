8d56ee0b6ab24e2d2143b6a14bfda0ecac977858
Nathanaelfs <feitosanathan10@gmail.com>
2024-08-22 20:19:24 -0300

    Add files via upload

---
+++ exerciciosPolkadotSemana4(Python)/01desvendando_o_Código_Secreto_da_Polkadot.py
@@ -0,0 +1,28 @@
+numeroInicio=int(input('digite o primeiro numero '))
+numeroFim=int(input('digite o segundo numero '))
+valorfinal=0
+def multiplo(numerador,divisor):
+    resultado=numerador%divisor
+    if resultado==0:
+        return True
+    else:
+        return False
+    
+for i in range(numeroInicio,numeroFim+1):
+    
+    multiplo3=multiplo(i,3)
+    multiplo5=multiplo(i,5)
+    print(f"{i}/3={multiplo3}\n {i}/5={multiplo5}")
+    if multiplo3 == True and multiplo5 ==True:
+        print(f'{i} foi ignorado')
+        break
+    else:
+        if multiplo3 == True:
+           valorfinal+=i
+           print(f'{i} foi somado')
+        else:
+         if multiplo5 == True:
+            valorfinal=valorfinal-i  
+            print(f'{i} foi subtraido')
+        
+print(valorfinal)
